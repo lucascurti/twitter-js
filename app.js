e8447d1495110f7b71875ab94af2048c13212b50
lucascurti <lucas.curti@gmail.com>
2018-01-24 15:21:31 -0300

    twitter version inicial

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 const express = require('express');
 const morgan = require('morgan');
+const nunjucks = require('nunjucks');
+const routes = require('./routes');
 
 const app = express(); // crea una instancia de una aplicación de express
+app.set('view engine', 'html'); // hace que res.render funcione con archivos html
+app.engine('html', nunjucks.render); // cuando le den archivos html a res.render, va a usar nunjucks
+// nunjucks.configure('views'); // apunta a nunjucks al directorio correcto para los templates
+nunjucks.configure('views', { noCache: true });
 
-app.use('/', function(req, res, next) {
-  // haz tu logueo aqui
-  // llama a `next`, o sino tu app va a ser un agujero negro - recibiendo pedidos pero nunca respondiendo adecuadamente.
-  next();
-});
 app.use(morgan('tiny'));
 
-app.use('/special', function(req, res, next) {
-  // haz tu logueo aquí
-  console.log('haz llegado al area magica');
-  // llama a `next`, o sino tu app va a ser un agujero negro - recibiendo pedidos pero nunca respondiendo adecuadamente.
-  next();
-});
+app.use('/', routes);
 
-app.get('/', function(req, res) {
-  res.send('Welcome');
-});
+// app.use('/special', function(req, res, next) {
+//   // haz tu logueo aquí
+//   console.log('haz llegado al area magica');
+//   // llama a `next`, o sino tu app va a ser un agujero negro - recibiendo pedidos pero nunca respondiendo adecuadamente.
+//   next();
+// });
+
+// app.get('/', function(req, res) {
+//   const people = [{ name: 'Full' }, { name: 'Stacker' }, { name: 'Son' }];
+//   res.render('index', { title: 'Hall of Fame', people: people });
+// });
 
 app.listen(3000);
